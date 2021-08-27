6eb87a6f34b966d529fac85c5396df15ae04e689
tis51T <88530493+tis51T@users.noreply.github.com>
2021-08-27 09:45:16 +0700

    Update main_character.py

--- main_character.py
+++ main_character.py
@@ -1,6 +1,7 @@
 from setting import Screen, Colors, FontType
 import pygame
 from pygame.locals import *
+
 pygame.init()
 
 
@@ -12,7 +13,7 @@ class Player():
         self.game_surface_x = 0
         self.game_surface_y = 0
         self.game_surface = pygame.Surface(
-            (self.game_surface_width, self.game_surface_height), pygame.SRCALPHA)
+            (self.game_surface_width, self.game_surface_height), SRCALPHA)
 
         self.theme_extend_left = False
         self.theme_extend_right = False
@@ -25,7 +26,7 @@ class Player():
         self.pos_y = 500 - self.surface_height
 
         self.surface = pygame.Surface(
-            (self.surface_width, self.surface_height), pygame.SRCALPHA)
+            (self.surface_width, self.surface_height), SRCALPHA)
 
         '''CUSTOMIZE PLAYER'S APPEARANCE'''
         # Legs
@@ -41,7 +42,7 @@ class Player():
         self.right_leg_x = self.left_leg_x + 5
         self.right_leg_y = self.left_leg_y
         pygame.draw.rect(self.surface, Colors.BLACK, (self.right_leg_x + 20,
-                         self.right_leg_y, self.leg_width, self.leg_height))
+                                                      self.right_leg_y, self.leg_width, self.leg_height))
 
         # Body
         self.body_x = self.right_leg_x + self.leg_width
@@ -62,7 +63,7 @@ class Player():
         pygame.draw.circle(self.surface, Colors.BLACK,
                            (self.eye_x - 22, self.eye_y), self.eye_rad_inner)
         pygame.draw.circle(self.surface, Colors.BLACK, (self.eye_x,
-                           self.eye_y), self.eye_rad_inner)
+                                                        self.eye_y), self.eye_rad_inner)
 
         # Arms
         self.arm_rad = 7
@@ -103,9 +104,6 @@ class Player():
         self.give_damage_left = False
         self.give_damage_right = False
 
-        # Money
-        self.money = 1000
-
         pass
 
     def draw(self):
@@ -114,82 +112,79 @@ class Player():
                     (self.game_surface_x, self.game_surface_y))
         pass
 
-    def update(self, status):
-        def movement():
-            #print(status.die)
-            if not status.die:
-                '''MOVING IN 4 DIRECTIONS'''
-                key_pressed = pygame.key.get_pressed()
-                # Moving Left
-                if key_pressed[pygame.K_a] and not self.theme_extend_left:
-                    self.pos_x -= self.speed
+    def update(self):
+        '''MOVING IN 4 DIRECTIONS'''
+        key_pressed = pygame.key.get_pressed()
+        # Moving Left
+        if key_pressed[pygame.K_a] and not self.theme_extend_left:
+            self.pos_x -= self.speed
 
-                # Moving Right
-                if key_pressed[pygame.K_d] and not self.theme_extend_right:
-                    self.pos_x += self.speed
+        # Moving Right
+        if key_pressed[pygame.K_d] and not self.theme_extend_right:
+            self.pos_x += self.speed
 
-                # Jumping & Falling
-                    # get key
-                if key_pressed[pygame.K_w] and self.jump == False and self.fall == False:
-                    self.jump = True
+        # Jumping & Falling
+        # get key
+        if key_pressed[pygame.K_w] and self.jump == False and self.fall == False:
+            self.jump = True
 
-                    # stand still -> jump
-                if self.jump == True and self.fall == False and self.jump_step < self.jump_limit:
-                    self.pos_y -= self.jump_speed
-                    self.jump_step += self.jump_speed
+            # stand still -> jump
+        if self.jump == True and self.fall == False and self.jump_step < self.jump_limit:
+            self.pos_y -= self.jump_speed
+            self.jump_step += self.jump_speed
 
-                    # jump -> fall
-                if self.jump_step >= self.jump_limit:
-                    self.jump = False
-                    self.fall = True
+            # jump -> fall
+        if self.jump_step >= self.jump_limit:
+            self.jump = False
+            self.fall = True
 
-                if self.jump == False and self.fall == True and self.jump_step > 0:
-                    self.pos_y += self.fall_speed
-                    self.jump_step -= self.fall_speed
+        if self.jump == False and self.fall == True and self.jump_step > 0:
+            self.pos_y += self.fall_speed
+            self.jump_step -= self.fall_speed
 
-                    # fall -> stand still
-                if self.jump_step <= 0:
-                    self.fall = False
+            # fall -> stand still
+        if self.jump_step <= 0:
+            self.fall = False
 
-                # Running
-                if key_pressed[pygame.K_LCTRL]:
-                    self.speed = self.run_speed
+        # Running
+        if key_pressed[pygame.K_LCTRL]:
+            self.speed = self.run_speed
+        else:
+            self.speed = self.init_speed
+
+        # Attacking
+        # get mouse pos
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                (mouseX, mouseY) = pygame.mouse.get_pos()
+
+                if mouseX < self.body_x + self.body_rad:
+                    self.give_damage_left = True
                 else:
-                    self.speed = self.init_speed
+                    self.give_damage_right = True
+            else:
+                self.give_damage_left = False
+                self.give_damage_right = False
+        # Unfinished
 
-                # Attacking
-                # get mouse pos
-                for event in pygame.event.get():
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        (mouseX, mouseY) = pygame.mouse.get_pos()
+        # Moving background while character moving near border
+        # CHECK LEFT BORDER
+        if self.pos_x < self.game_surface_x + 100 and key_pressed[K_a]:
+            self.theme_extend_left = True
+        else:
+            self.theme_extend_left = False
 
-                        if mouseX < self.body_x + self.body_rad:
-                            self.give_damage_left = True
-                        else:
-                            self.give_damage_right = True
-                    else:
-                        self.give_damage_left = False
-                        self.give_damage_right = False
-                # Unfinished
+        # CHECK RIGHT BORDER
+        if self.pos_x + self.surface_width > self.game_surface_x + self.game_surface_width - 100 and key_pressed[K_d]:
+            self.theme_extend_right = True
+        else:
+            self.theme_extend_right = False
 
-                # Moving background while character moving near border
-                # CHECK LEFT BORDER
-                if self.pos_x < self.game_surface_x + 100:
-                    self.theme_extend_left = True
-                else:
-                    self.theme_extend_left = False
+        # DEBUGGING
+        '''print(str(self.pos_x) + ' ' + str(self.theme_extend_left) +
+              ' ' + str(self.theme_extend_right))'''
 
-                # CHECK RIGHT BORDER
-                if self.pos_x + self.surface_width > self.game_surface_x + self.game_surface_width - 100:
-                    self.theme_extend_right = True
-                else:
-                    self.theme_extend_right = False
-                
-                # DEBUGGING
-                '''print(str(self.pos_x) + ' ' + str(self.theme_extend_left) +
-                    ' ' + str(self.theme_extend_right))'''
-        movement()
-        pass
+    pass
 
 
 class StatusBar():
@@ -209,24 +204,36 @@ class StatusBar():
         self.frame_y = 5
         self.frame_width = self.surface_width - 20
         self.frame_height = self.surface_height - 10
+        pygame.draw.rect(self.surface, Colors.WHITE, [self.frame_x,
+                                                      self.frame_y, self.frame_width, self.frame_height])
 
-        # hp bar (hp range: 50 -> 190)
+        # hp bar
         self.hp_x = self.frame_x + 45
         self.hp_y = self.frame_y + 10
-        self.max_hp = 190
+        self.hp_width = 70
         self.hp_height = 28
-        self.current_hp = self.max_hp
-        self.get_damage = False
-        self.die = False
+        pygame.draw.rect(self.surface, Colors.RED, (self.hp_x,
+                                                    self.hp_y, self.hp_width, self.hp_height))
 
-        # mp bar (mp range: 50 -> 190)
+        # mp bar
         self.mp_x = self.hp_x
         self.mp_y = self.hp_y + 40
-        self.max_mp = 50
-        self.current_mp = self.max_mp
+        self.mp_width = 70
         self.mp_height = self.hp_height
+        pygame.draw.rect(self.surface, Colors.BLUE, (self.mp_x,
+                                                     self.mp_y, self.mp_width, self.mp_height))
+
+        '''VARIABLE FOR MECHANISM'''
+        # HP/MP
+        self.max_hp = self.hp_width
+        self.hp_amount = self.max_hp
+        self.get_damage = False
+
+        self.max_mp = self.mp_width
+        self.mp_amount = self.max_mp
         self.use_mana = False
-        self.out_of_mana = False
+
+        self.die = False
 
         pass
 
@@ -234,34 +241,20 @@ class StatusBar():
         # visualize images
         Screen.blit(self.surface, (self.pos_x, self.pos_y))
 
-        # draw frame of status bar
-        pygame.draw.rect(self.surface, Colors.WHITE, (self.frame_x,
-                         self.frame_y, self.frame_width, self.frame_height))
-
         # print hp text
         Screen.blit(pygame.font.Font.render(
             FontType.FONT3, 'HP: ', True, Colors.LIGHTBROWN), (self.hp_x - 25, self.hp_y + self.hp_height / 2 - 3))
         Screen.blit(pygame.font.Font.render(
-            FontType.FONT3, str(self.current_hp) + '/' + str(self.max_hp), True, Colors.WHITE), (self.hp_x + 12, self.hp_y + self.hp_height / 2 - 3))
-
-        # draw hp bar
-        pygame.draw.rect(self.surface, Colors.BLACK, (self.hp_x,
-                         self.hp_y, self.max_hp, self.hp_height))
-        pygame.draw.rect(self.surface, Colors.RED, (self.hp_x,
-                         self.hp_y, self.current_hp, self.hp_height))
+            FontType.FONT3, str(self.hp_amount) + '/' + str(self.max_hp), True, Colors.WHITE),
+            (self.hp_x + 10, self.hp_y + self.hp_height / 2 - 3))
 
         # print mp text
+
         Screen.blit(pygame.font.Font.render(
             FontType.FONT3, 'MP: ', True, Colors.LIGHTBROWN), (self.mp_x - 25, self.mp_y + self.mp_height / 2 - 3))
         Screen.blit(pygame.font.Font.render(
-            FontType.FONT3, str(self.current_mp) + '/' + str(self.max_mp), True, Colors.WHITE), (self.mp_x + 12, self.mp_y + self.mp_height / 2 - 3))
-
-        # draw mp bar
-        pygame.draw.rect(self.surface, Colors.BLACK, (self.mp_x,
-                         self.mp_y, self.max_mp, self.mp_height))
-        pygame.draw.rect(self.surface, Colors.BLUE, (self.mp_x,
-                         self.mp_y, self.current_mp, self.mp_height))
-
+            FontType.FONT3, str(self.mp_amount) + '/' + str(self.max_mp), True, Colors.WHITE),
+            (self.mp_x + 10, self.mp_y + self.mp_height / 2 - 3))
         pass
 
     def update(self):
@@ -280,32 +273,18 @@ class StatusBar():
         else:
             self.use_mana = False
 
-        if self.get_damage:
-            self.current_hp -= 1
-            if self.current_hp <= 0:
-                self.die = True
-                self.current_hp = 0
+        if self.get_damage == True and self.hp_width > 0:
+            self.hp_width -= 1
+            self.hp_amount -= 1
+        else:
+            self.die = True
 
-        if self.use_mana == True:
-            self.current_mp -= 1
-            if self.current_mp <= 0:
-                self.out_of_mana = True
-                self.current_mp = 0
+        if self.use_mana == True and self.mp_width > 0:
+            self.mp_width -= 1
+            self.mp_amount -= 1
 
         if self.die:
-            pass
-        # DEBUG
-        #print(self.current_hp, ' ', self.current_mp)
+            self.speed = 0
+
         # Unfinished
-        pass
-
-
-class Inventory():
-    def __init__(self):
-        pass
-
-    def draw(self):
-        pass
-
-    def update(self):
         pass
